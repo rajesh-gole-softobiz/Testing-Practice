50ac2ee48db377851e43857f140e4128cfe7a1ad
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-22 14:27:40 +0530

    capitalize failed test case

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -1,37 +1,41 @@
 import capitalize from './capitalize';
 
-test('"test" should become "Test"', () => {
+test( 'test-case-1',() => {
   expect(capitalize('test')).toBe('Test');
 });
 
-test('"Test" should become "Test"', () => {
+test('test-case-2', () => {
   expect(capitalize('Test')).toBe('Test');
 });
 
-test('"TEST" should become "TEST"', () => {
+test('test-case-3', () => {
   expect(capitalize('TEST')).toBe('TEST');
 });
 
-test('"three word test" should become "Three word test"', () => {
+test('test-case-4', () => {
   expect(capitalize('three word test')).toBe('Three word test');
 });
 
-test('"12@#abcd@#34" should become "12@#Abcd@#34"', () => {
+test('test-case-5', () => {
   expect(capitalize('12@#abcd@#34')).toBe('12@#Abcd@#34');
 });
 
-test('"12@#Abcd@#34" should become "12@#Abcd@#34"', () => {
+test('test-case-6', () => {
   expect(capitalize('12@#Abcd@#34')).toBe('12@#Abcd@#34');
 });
 
-test('"1234" should become "no valid characters"', () => {
+test('test-case-7', () => {
   expect(capitalize('1234')).toBe('no valid characters');
 });
 
-test('"@#$" should become "no valid characters"', () => {
+test('test-case-8', () => {
   expect(capitalize('@#$')).toBe('no valid characters');
 });
 
-test('1234 should become "no valid characters"', () => {
+test('test-case-9', () => {
   expect(capitalize(1234)).toBe('no valid characters');
+});
+
+test('test-case-10', () => {
+  expect(capitalize('  aL2')).toBe('failed test case');
 });
