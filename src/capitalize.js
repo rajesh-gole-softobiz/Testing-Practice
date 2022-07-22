ccdc38da2b4264d1510f6269f5b56c50ee1fefc2
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-22 14:14:49 +0530

    capitalize

---
+++ src/capitalize.js
@@ -0,0 +1,35 @@
+// takes a string and returns it with the first character capitalized
+const capitalize = (string) => {
+    for (let i = 0; i < string.length; i++) {
+      if (isUpperCase(string[i])) {
+        return string;
+      } else if (isLowerCase(string[i])) {
+        let newString =
+          string.slice(0, i) +
+          string[i].toUpperCase() +
+          string.slice(i + 1, string.length + 1);
+  
+        return newString;
+      }
+    }
+    return 'no valid characters';
+  };
+  
+  // If char is in lowercase return true else return false
+  const isLowerCase = (char) => {
+    if (char == char.toLowerCase() && char != char.toUpperCase()) {
+      return true;
+    }
+    return false;
+  };
+  
+
+  // If char is in uppercase return true else return false
+  const isUpperCase = (char) => {
+    if (char == char.toUpperCase() && char != char.toLowerCase()) {
+      return true;
+    }
+    return false;
+  };
+  
+  export default capitalize;  
